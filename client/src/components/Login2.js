133db0a3068b48bbeccd247c003a8282bd461896
François Costa <frans1998@gmail.com>
2023-03-20 19:48:31 +0100

    modif axios

--- client/src/components/Login2.js
+++ client/src/components/Login2.js
@@ -2,29 +2,31 @@ import '../css/Login.css'
 
 export default function Login2() {
 
-    // let handleLogin = () => {
-    //     const authorizeUrl = "https://accounts.spotify.com/authorize?client_id=9e6a9593ba0c40e8be113a49ac48f487&response_type=code&redirect_uri=http://localhost:3000/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-private%20playlist-modify-public";
-    //     window.location.href = authorizeUrl;
-    // };
-    // //ADDED check if dev
-    // const loc = window.location;
-    // if(loc.hostname === 'localhost'){
-    //     handleLogin = () => {
-    //         const authorizeUrl = "https://accounts.spotify.com/authorize?client_id=9e6a9593ba0c40e8be113a49ac48f487&response_type=code&redirect_uri=http://localhost:3000/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-private%20playlist-modify-public";
-    //         window.location.href = authorizeUrl;
-    //     };
-    // }
-    // else{
-    //     handleLogin = () => {
-    //         //client URL
-    //         const authorizeUrl = "https://accounts.spotify.com/authorize?client_id=9e6a9593ba0c40e8be113a49ac48f487&response_type=code&redirect_uri=https://main--lucky-biscuit-d37265.netlify.app/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-private%20playlist-modify-public";
-    //         window.location.href = authorizeUrl;
-    //     };
-    // }
-    const handleLogin = () => {
+    let handleLogin = () => {
         const authorizeUrl = "https://accounts.spotify.com/authorize?client_id=9e6a9593ba0c40e8be113a49ac48f487&response_type=code&redirect_uri=http://localhost:3000/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-private%20playlist-modify-public";
         window.location.href = authorizeUrl;
     };
+    //ADDED check if dev
+    const loc = window.location;
+    if(loc.hostname === 'localhost'){
+        handleLogin = () => {
+            const authorizeUrl = "https://accounts.spotify.com/authorize?client_id=9e6a9593ba0c40e8be113a49ac48f487&response_type=code&redirect_uri=http://localhost:3000/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-private%20playlist-modify-public";
+            window.location.href = authorizeUrl;
+        };
+    }
+    else{
+        handleLogin = () => {
+            //client URL
+            console.log("CLIENT GOGOGO");
+            const authorizeUrl = "https://accounts.spotify.com/authorize?client_id=9e6a9593ba0c40e8be113a49ac48f487&response_type=code&redirect_uri=https://main--lucky-biscuit-d37265.netlify.app/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-private%20playlist-modify-public";
+            window.location.href = authorizeUrl;
+            console.log("CLIENT GOGOGO 2");
+        };
+    }
+    // const handleLogin = () => {
+    //     const authorizeUrl = "https://accounts.spotify.com/authorize?client_id=9e6a9593ba0c40e8be113a49ac48f487&response_type=code&redirect_uri=http://localhost:3000/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-private%20playlist-modify-public";
+    //     window.location.href = authorizeUrl;
+    // };
 
     return (
         <div>
